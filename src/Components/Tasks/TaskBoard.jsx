ce967702999331c85a64f42e7a1f94d3be6e5fcf
Shadman-Shakib-27 <s.s.a.27.8.1999@gmail.com>
2024-10-12 03:13:40 +0600

    Modal Add Function Done.

--- src/Components/Tasks/TaskBoard.jsx
+++ src/Components/Tasks/TaskBoard.jsx
@@ -1,24 +1,45 @@
+import { useState } from "react";
+import AddTaskModal from "./AddTaskModal";
 import SearchTask from "./SearchTask";
 import TaskAction from "./TaskAction";
 import TaskList from "./TaskList";
 
 const TaskBoard = () => {
+  const defaultTask = {
+    id: crypto.randomUUID(),
+    title: "Learn React",
+    description:
+      "I want to Learn React such that i can treat it like my slave and make it do whatever i want to do.",
+    tags: ["Web", "React", "Js"],
+    priority: "High",
+    isFavourite: true,
+  };
+
+  const [tasks, setTasks] = useState([defaultTask]);
+  const [showAddModal, setShowAddModal] = useState(false);
+
+  //Some Issue To Realize, Live Session
+  const handleAddTask = (newTask) => {
+    console.log("Add", newTask);
+    setTasks(newTask);
+    setShowAddModal(false);
+  };
+
   return (
     <>
       <section className="mb-20" id="tasks">
+        {showAddModal && <AddTaskModal onSave={handleAddTask} />}
         <div className="container">
-          {/* Task Search Component */}
           <SearchTask />
-          {/* Task Search Component */}
 
           <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
-            {/* Task Action Component */}
-            <TaskAction />
-            {/* Task Action Component */}
+            <TaskAction
+              onAddClick={() => {
+                setShowAddModal(true);
+              }}
+            />
 
-            {/* Task List Component */}
-            <TaskList />
-            {/* Task List Component */}
+            <TaskList tasks={tasks} />
           </div>
         </div>
       </section>
